d883ea39c575acc10540981db038556f09bc752a
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-02-15 09:19:39 +0900

    디렉토리 세분화 및 제이쿼리 예제 추가

---
+++ 05_javascript&jQuery/jquery/04.jquery_animate/animation01/js/script.js
@@ -0,0 +1,69 @@
+﻿
+jQuery(document).ready(function(){
+	ff();
+	text_anii();
+	 
+	   
+	function ff(){
+		setTimeout(function(){
+			first();
+			text_ani();
+			function first(){
+						setTimeout(function(){
+						var spans=$('.container p span');
+						var spani=spans.eq(0);
+							 
+						spani.delay(0).animate({top: -20},500);
+						spani.delay(0).animate({top: 0},500).stop();
+					},0);
+				};
+			  
+				var i=1; //변수 안에다쓰고 밖에다쓰고 그 차이 중요함
+				function text_ani(){      
+					setInterval(function(){   
+						var spans=$('.container p span');
+						var spanii=spans.eq(i);
+						var x=i*200;   
+					
+						i++;
+												  
+						spanii.delay(x).animate({top: -20},500);
+						spanii.animate({top: 0},500);
+					},0);         
+				 }; 
+		},0)
+	}   
+	   
+	function text_anii(){   
+	   setTimeout(function(){
+			setInterval(function(){
+				first();
+				text_ani();
+				function first(){
+						setTimeout(function(){
+						var spans=$('.container p span');
+						var spani=spans.eq(0);
+							 
+						spani.delay(0).animate({top: -20},500);
+						spani.delay(0).animate({top: 0},500).stop();
+					},50);
+				};
+			  
+				var i=1; //변수 안에다쓰고 밖에다쓰고 그 차이 중요함
+				function text_ani(){      
+					setInterval(function(){   
+						var spans=$('.container p span');
+						var spanii=spans.eq(i);
+						var x=i*200;   
+					
+						i++;
+												  
+						spanii.delay(x).animate({top: -20},500);
+						spanii.animate({top: 0},500);
+					},0);         
+				 }; 
+			},5000);
+		},0);
+	};   
+
+});

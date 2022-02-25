1297582a8a24bad81e5593089babe526809d43cb
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-02-25 14:38:11 +0900

    오퍼서티 예제

---
+++ 05_javascript&jQuery/jquery/06.jquery_effect/13_이미지4개_오퍼서티_회전/js/script.js
@@ -0,0 +1,20 @@
+let box=$('.stories .contents');
+let col1 = 0;
+let col2 = 1;
+let col3 = 2;
+let col4 = 3;
+let col5 = 4;
+let col6 = 5;
+let col7 = 6;
+let col8 = 7;
+let col9 = 8;
+let timer;
+
+// timer=setInterval(events,200);
+
+// function events(){
+//     box.eq(col1).stop().css({transform:'scale(.9)',zIndex:0}).animate({left:"10%", top:50, opacity:.5, marginLeft:0},1000);
+//     box.eq(col2).stop().css({transform:'scale(.8)',zIndex:0}).animate({left:"50%", opacity:.3, marginLeft:"-11%"},1000);
+//     box.eq(col3).stop().css({transform:'scale(.9)',zIndex:0}).animate({left:"68%", opacity:.5, marginLeft:0, top:50 },1000);
+//     box.eq(col4).stop().css({transform:'scale(1)',zIndex:1}).animate({left:"50%", opacity:1, marginLeft:"-11%", top:100 },1000);
+// }

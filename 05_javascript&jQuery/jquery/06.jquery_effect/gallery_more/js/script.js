5fb89e77cd73e6d36669024b29e8895d6acd9ad4
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-03-03 10:42:44 +0900

    겔러리 더보기 예제 업로드

---
+++ 05_javascript&jQuery/jquery/06.jquery_effect/gallery_more/js/script.js
@@ -0,0 +1,14 @@
+
+const $container=$('.gallery'); //이미지들을 감싸는 겔러리
+const $loadMoreBtn=$('.load-more'); //더보기버튼
+let $addItemCount=8; //클릭할때 나올 이미지 갯수
+let $added=0;//처음보이는 것 리스트 항복 모두 로드하면 버튼이 사라지게 할 용도
+let $allData = [];
+
+// $.getJSON(파일경로, 함수);
+$.getJSON('./data/content.json', function(data){
+});
+function initGallery(data){
+    $allData=data;
+    console.log($allData);
+};

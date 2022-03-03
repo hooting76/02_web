096ac7d75981418adfcd97f5dd9abed340f23917
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-03-03 12:58:19 +0900

    간격수정

--- 05_javascript&jQuery/jquery/06.jquery_effect/gallery_more/js/script.js
+++ 05_javascript&jQuery/jquery/06.jquery_effect/gallery_more/js/script.js
@@ -1,11 +1,9 @@
+const $container=$('.gallery');//이미지들을 감싸는 갤러리
+const $loadMoreBtn=$('.load-more');//더보기 버튼
+let $addItemCount=8;//클릭할때마도 나오는 갯수
+let $added=0;//처음보이는 것 리스트 항목 모두 로드하면 버튼이 사라지게 할 용도
+let $allData=[];
 
-const $container=$('.gallery'); //이미지들을 감싸는 겔러리
-const $loadMoreBtn=$('.load-more'); //더보기버튼
-let $addItemCount=8; //클릭할때 나올 이미지 갯수
-let $added=0;//처음보이는 것 리스트 항복 모두 로드하면 버튼이 사라지게 할 용도
-let $allData = [];
-
-// $.getJSON(파일경로, 함수);
 $.getJSON('./data/content.json', function(data){
     initGallery(data);
 });
@@ -14,45 +12,44 @@ $container.masonry({
     // options
     itemSelector: '.gallery-item',
     columnWidth: 210,
-    gutter: 40
-  });
-
+    gutter:30
+});
 function initGallery(data){
     $allData=data;
-    // console.log($allData);
-    addItem()
+    //console.log($allData);
+    addItem();
     $loadMoreBtn.click(function(){
         addItem();
     });
-};
 
+}
 function addItem(){
     let elements=[];
-    let sliceData;
-    sliceData=$allData.slice($added, $added+=$addItemCount);
-    $.each(sliceData, function(idx, item){
-        let itemHTML= 
-        '<li class="gallery-item">'+
-            '<a href="'+item.images.large+'">'+
-                    '<figure>'+
-                        '<img src="'+item.images.thumb+'"alt="'+ item.title+'">'+
-                        '<figcaption>'+item.title+'</figcaption>'+
-                    '</figure>'+
-                '</a>'+
-            '</li>';
-            elements.push($(itemHTML).get(0))
-    });
+    let slicedDate;
+    slicedDate=$allData.slice($added, $added += $addItemCount);
+    $.each(slicedDate, function(idx, item){
+        let itemHTML=
+        '<li class="gallery-item">' + 
+            '<a href="'+item.images.large+'">' +
+                '<figure>' +
+                    '<img src="'+item.images.thumb+'" alt="'+item.title+'">'+
+                    '<figcaption>'+item.title+'</figcaption>'+
+                '</figure>'+
+            '</a>'+
+        '</li>';
+        elements.push($(itemHTML).get(0))
+    })
     $container.append(elements);
-    $added +=sliceData.length;
+    $added += slicedDate.length;
 
     if($added < $allData.length){
-        $loadMoreBtn.show();
+        $loadMoreBtn.show()
     }else{
-        $loadMoreBtn.hide();
+        $loadMoreBtn.hide()
     }
 
     $container.imagesLoaded( function() {
-        $container.masonry('append',elements);
-      });
+        $container.masonry('appended', elements);
+    });
 }
 

0b9c400692efa81b6a7977fca23f7e0ce81038a2
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-03-15 14:35:33 +0900

    목업 재수정

--- 05_javascript&jQuery/jquery/mobile_mokup/js/script.js
+++ 05_javascript&jQuery/jquery/mobile_mokup/js/script.js
@@ -1,6 +1,18 @@
-if (Modernizr.touchevents) {
-    // supported
-    location.href='https://hooting76.github.io/02_web/05_javascript&jQuery/jquery/mobile_mokup/index.html'
-  } else {
-    // not-supported
-  }
+var deviceAgent = navigator.userAgent.toLowerCase();
+
+var isTouchDevice = Modernizr.touch ||
+(deviceAgent.match(/(iphone|ipod|ipad)/) ||
+deviceAgent.match(/(android)/)  ||
+deviceAgent.match(/(iemobile)/) ||
+deviceAgent.match(/iphone/i) ||
+deviceAgent.match(/ipad/i) ||
+deviceAgent.match(/ipod/i) ||
+deviceAgent.match(/blackberry/i) ||
+deviceAgent.match(/bada/i));
+
+if (isTouchDevice) {
+        //Do something touchy
+        location.href='https://hooting76.github.io/02_web/05_javascript&jQuery/jquery/mobile_mokup/index.html';
+    } else {
+        //Can't touch this
+    }

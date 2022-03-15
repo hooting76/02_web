fe038900464b47d5d48c210fa56abc53f74786ad
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-03-15 13:04:41 +0900

    모바일목업 추가수정

--- 05_javascript&jQuery/jquery/mobile_mokup/js/script.js
+++ 05_javascript&jQuery/jquery/mobile_mokup/js/script.js
@@ -1,6 +1,6 @@
 if (Modernizr.touchevents) {
     // supported
-    location.href='./mobile/index.html'
+    location.href='https://hooting76.github.io/02_web/05_javascript&jQuery/jquery/mobile_mokup/index.html'
   } else {
     // not-supported
   }
